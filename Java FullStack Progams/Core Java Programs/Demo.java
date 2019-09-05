74cc8835f41989bf1463b0962efa5a443e3431b6
TrainerA@hexaware.com <TrainerA@hexaware.com>
2019-09-05 17:38:41 +0530

    Day 2 Notes - 05-09-2019

--- Java FullStack Progams/Core Java Programs/Demo.java
+++ Java FullStack Progams/Core Java Programs/Demo.java
@@ -1,33 +1,35 @@
-import java.util.Scanner;
-class Demo {
-	public static void main(String args[]){
-	Scanner obj = new Scanner(System.in);
-	System.out.println("How many record do you wan to store?");
-	int n = obj.nextInt();
-	int id[]=new int[n];
-	String name[]=new String[n];
-	float salary[]=new float[n];
-	System.out.println("Enter the records one by one");
-	//Receive the values 
-	for(int i=0;i<n;i++) {
-	System.out.println("Enter the id");
-	id[i]=obj.nextInt();
-	obj.nextLine();	//use to hold the enter key
-	System.out.println("Enter the name");
-		name[i]=obj.nextLine();
-	System.out.println("Enter the salary");
-	salary[i]=obj.nextFloat();
+interface A {
+	int add(int x, int y);
+}
+interface B {
+	int sub(int x, int y);
+}
+class Server implements A,B{
+	public int add(int x, int y){
+		return x+y;
 	}
-	//Business logic 
-	for(int i=0;i<n;i++) {
-		float hra, da,pf;
-		hra = salary[i]*0.10f;
-		da = salary[i]*0.05f;
-		pf = salary[i]*0.07f;
-	salary[i]=salary[i]+hra +da -pf;		
-	}		
-	//Display Records;
-	for(int i=0;i<n;i++) {
-System.out.println("id is"+id[i]+" Name is"+name[i]+" Salary is"+salary[i]);		}
+	public int sub(int x, int y){
+		return x-y;
+	}
+	public void ownMethod() {
+		System.out.println("Server own method");
 	}
 }
+class Demo {
+	public static void main(String args[]){
+	Server ss = new Server();
+	ss.add(10,20);	ss.sub(20,10);	ss.ownMethod();
+	A obj1 = new Server();	
+	B obj2 = new Server();
+	System.out.println(obj1.add(10,20));	
+	System.out.println(obj2.sub(10,20));	
+	}
+}
+
+
+
+
+
+
+
+
